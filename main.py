4d3c7763922ab39a6e99ca5be423898ed7d881c4
abdo60602 <abdala.darwish26@gmail.com>
2018-12-05 23:18:09 +0200

    Add test print and button login and solve code grid

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from Tkinter import *
 
-class takelook:
-    def __init__(self, master):
-        self.master = master
-        master.title("Takelook")
+class Application(Frame):
 
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+
+    def createWidgets(self):
+        self.QUIT = Button(self, text='Login', fg='red', command=self.test, font=("Arial", 15))
+        self.QUIT.grid(ipadx = '50px')
+
+    def test(self):
+    	print 'this Ok'
     
 
 root = Tk()
 root.geometry("400x450")
-my_gui = takelook(root)
-root.mainloop()
+app = Application(master=root)
+app.mainloop()
