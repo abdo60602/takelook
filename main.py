5651f688687c4c94dadfe1b3bc39a169951b3c9c
abdo60602 <abdala.darwish26@gmail.com>
2018-12-09 13:04:26 +0200

    User login test

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Application(Frame):
         
         self.user_name = Entry(self, font=("Arial", 15))
         self.user_name.grid(pady=10, row= 0,padx=120)
-        self.user_name.bind("<Return>",self.test)
+        self.user_name.bind("<Return>",self.test_login)
         
 
     	self.password_ = Label(self, text='Password  :',font=("Arial", 15))
@@ -22,11 +22,16 @@ class Application(Frame):
         
         self.password = Entry(self, font=("Arial", 15),show="-")
         self.password.grid(row=1,pady=10)
-        self.password.bind("<Return>",self.test)
+        self.password.bind("<Return>",self.test_login)
 
-        self.QUIT = Button(self, text='Login', fg='red', command= self.test, font=("Arial", 15))
+        self.QUIT = Button(self, text='Login', fg='red', command= self.test_login, font=("Arial", 15))
         self.QUIT.grid(ipadx =165)
 
+    def test_login(self, *event):
+    	if self.user_name.get() == 'admin' and self.password.get() == 'admin':
+    		print 'Right Password'
+    	else:
+    		print 'Retry'
 
 
     def test(self, *event):
