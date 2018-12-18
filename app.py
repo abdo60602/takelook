127f62ba4f9e4e98aaa8d137abdcd3c3af9c385d
abdo60602 <abdala.darwish26@gmail.com>
2018-12-18 19:12:45 +0200

    Solve "self" frime background

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ root = Tk()
 
 root.maxsize(width=450, height=470)
 root.minsize(width=450, height=470)
-root.configure(background='#00ff38')
+root.configure(background='black')
 app = main.Application(master=root)
 app.mainloop()
--- main.py
+++ main.py
@@ -7,28 +7,29 @@ class Application(Frame):
 
     def __init__(self, master=None):    	
         Frame.__init__(self, master)
-        self.pack()
+        self.configure(bg='black')
+        self.grid()
         self.createWidgets()
 
 
     def createWidgets(self):
-    	self.user_name_ = Label(self, text='User Name:',font=("Arial", 15))
+    	self.user_name_ = Label(self,fg='#8bc900',bg='black', text='User Name:',font=("Arial", 15))
         self.user_name_.grid(sticky=W)
         
-        self.user_name = Entry(self, font=("Arial", 15))
+        self.user_name = Entry(self, font=("Arial", 15),fg='#8bc900')
         self.user_name.grid(pady=10, row= 0,padx=120)
         self.user_name.bind("<Return>",self.test_login)
         
 
-    	self.password_ = Label(self, text='Password  :',font=("Arial", 15))
+    	self.password_ = Label(self,bg='black', text='Password  :',font=("Arial", 15),fg='#8bc900')
         self.password_.grid(sticky=W)
         
-        self.password = Entry(self, font=("Arial", 15),show="-")
+        self.password = Entry(self, font=("Arial", 15),show="-",fg='#8bc900')
         self.password.grid(row=1,pady=10)
         self.password.bind("<Return>",self.test_login)
 
         self.login = Button(self, text='Login', fg='red', command= self.test_login, font=("Arial", 15))
-        self.login.grid(ipadx =165, rowspan=50)
+        self.login.grid(ipadx =70, rowspan=50)
 
     def test_login(self, *event):
     	if self.user_name.get() == 'admin' and self.password.get() == 'admin':
